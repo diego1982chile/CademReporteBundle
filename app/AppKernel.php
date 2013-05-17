8b9f1f1ab587f34915005edddec480cdae2904a3
Jonnatan Oyarzún <jonsxaero@gmail.com>
2013-05-17 14:16:22 -0400

    Actualizo a SF v2.3-RC1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,12 +16,12 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            // new JMS\AopBundle\JMSAopBundle(),
+            // new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            // new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Cadem\ReporteBundle\CademReporteBundle(),
-	    new FOS\UserBundle\FOSUserBundle(),
-	    new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
+			new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
